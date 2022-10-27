3d98238b557afbc9bf1b50e25643f0f561747d96
Sagar Shah <sa64r.shah@gmail.com>
2022-10-27 22:29:48 +0100

    iniital commit

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    isbn: String
+    title: String
+    author: String
+  }
+  type Cars {
+    id: String
+    make: String
+    model: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+    cars: [Cars]
+    car(id: String): Cars
+    book(isbn: String): Book
+  }
+`;
+
+const books = [
+    {
+        isbn: '978-1593275846',
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+        isbn: '978-1503261969',
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  const cars = [
+    {
+        id: '1',
+        make: 'Ford',
+        model: 'Mustang',
+    },
+    {
+        id: '2',
+        make: 'Chevy',
+        model: 'Camaro',
+    },
+  ]
+
+
+  // Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+      cars: () => cars,
+        car: (parent, args, context, info) => {
+            return cars.find(car => car.id === args.id);
+        },
+        book: (parent, args, context, info) => {
+            return books.find(book => book.isbn === args.isbn);
+        }
+    }
+};
+
+  // The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
